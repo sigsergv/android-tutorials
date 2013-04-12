c859c4c8edead8341caa4fd5986a054e5cd406da
Sergey Stolyarov <sergei@regolit.com>
2013-04-13 00:15:20 +0700

    Updating package name.

--- listview-simple-demo/src/com/regolit/android/listview_simple_demo/MainActivity.java
+++ listview-simple-demo/src/com/regolit/android/listview_simple_demo/MainActivity.java
@@ -1,16 +1,21 @@
 package com.regolit.android.listview_simple_demo;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 
-import com.regolit.android.demo.R;
+import com.regolit.android.listview_simple_demo.R;
 
 public class MainActivity extends Activity {
 	protected ArrayAdapter<String> mAdapter;
@@ -49,6 +54,43 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	@Override
+	protected void onStop() {
+		super.onStop();
+		
+		// save
+		saveListViewLines();
+	}
+	
+	protected void saveListViewLines() {
+		FileOutputStream fos;
+		try {
+			// открываем файл в приватном пространстве приложения
+			fos = openFileOutput("listview-lines.txt", Context.MODE_PRIVATE);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return;
+		}
+		int cnt = mAdapter.getCount();
+		String item;
+		
+		for (int i=0; i<cnt; ++i) {
+			try {
+				item = mAdapter.getItem(i) + "\n";
+				fos.write( item.getBytes("UTF-8") );
+			} catch (IOException e) {
+				e.printStackTrace();
+				return;
+			}
+		}
+		try {
+			fos.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
